de47588dc74127b47b8e7dcc069f2a28e38ad707
Oscar Hafner <ohgm.dev@gmail.com>
2024-01-14 17:31:27 +0100

    a full de mobile

--- app.vue
+++ app.vue
@@ -7,14 +7,14 @@
           <li><a href="#inicio">Inicio</a></li>
           <li><a href="#valores">Nuestros valores</a></li>
           <li><a href="#fermin">Sobre Fermín</a></li>
-          <li><a href="#contacto">Contacto</a></li>
+          <li><a href="#info">Información de Campaña</a></li>
         </ul>
       </nav>
     </header>
 
     <main class="pt-10 pb-20 flex flex-col flex-1 overflow-auto h-100">
-      <section id="inicio" class="mt-10 mb-20 p-10 rounded w-11/12 h-100 mx-auto bg-andalucia-gold-light">
-        <div class="h-90" style="height:400px;" >
+      <section id="inicio" class="mt-20 mb-20 p-10 rounded w-11/12 h-100 mx-auto bg-andalucia-gold-light">
+        <div class="h-90" style="height:400px;">
           <h2 class="text-2xl text-andalucia-green-dark">La confianza que necesitas</h2>
         </div>
       </section>
@@ -24,21 +24,33 @@
           <section id="valores" class="p-10">
             <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               <!-- Valor 1 -->
-              <div class="h-80 bg-andalucia-green-dark p-6 rounded-lg shadow-lg">
+              <div class="bg-andalucia-green-dark p-6 rounded-lg shadow-lg">
                 <h4 class="text-xl font-bold text-white">Andalucía Primero: Firmeza y Futuro</h4>
-                <p class="text-white">Unidos en la determinación de llevar a nuestra tierra hacia adelante.</p>
+                <p class="text-white text-justify text-lg my-10">
+                  Andalucía está en el epicentro de nuestro universo y no vamos a parar hasta llevarla hacia un futuro que
+                  nos deslumbre! Con una firmeza que sacude los cimientos y un compromiso que quema como el sol andaluz,
+                  ¡estamos en marcha hacia un mañana grandioso!
+                </p>
               </div>
 
               <!-- Valor 2 -->
               <div class="bg-andalucia-gold-dark p-6 rounded-lg shadow-lg">
                 <h4 class="text-xl font-bold text-white">Prosperidad Inquebrantable</h4>
-                <p class="text-white">Nuestra fuerza radica en el progreso económico y la autosuficiencia regional.</p>
+                <p class="text-andalucia-gold-light text-justify text-lg my-10">
+                  ¡Aquí no se rompe ni una sola fibra de nuestra inquebrantable determinación! Con el progreso económico
+                  como un martillo que forja el futuro y la autosuficiencia regional como nuestro estandarte,
+                  ¡alcanzaremos una prosperidad que hará temblar al mundo entero!
+                </p>
               </div>
 
               <!-- Valor 3 -->
               <div class="bg-andalucia-green-light p-6 rounded-lg shadow-lg">
                 <h4 class="text-xl font-bold text-andalucia-green-dark">Tradición y Revolución</h4>
-                <p class="text-andalucia-green-dark">Honrando nuestras raíces, abrazando el cambio audazmente.</p>
+                <p class="text-andalucia-green-dark text-justify text-lg my-10">
+                  Somos como el vino de Jerez, madurados en la tradición y fermentando la revolución. Honramos nuestro
+                  pasado con un brindis audaz hacia el futuro. ¡Andalucía, estamos dispuestos a incendiar el camino con la
+                  chispa de la innovación y la llama de la tradición!
+                </p>
               </div>
 
               <!-- Otros valores -->
@@ -48,47 +60,46 @@
         </div>
       </section>
 
-      <section id="fermin" class="mx-10 text-lg mt-5 mb-10">
-        <h3 class="text-3xl">Nuestra Chapa</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam felis lectus, pharetra et nulla sed, laoreet
-          cursus arcu. Morbi imperdiet, risus accumsan dapibus varius, nibh odio pellentesque ligula, eu gravida purus
-          lacus in neque. Maecenas posuere viverra urna commodo consectetur. Nullam gravida diam eget odio hendrerit, sit
-          amet lacinia ex posuere. Morbi a facilisis lacus. Morbi cursus neque et molestie dapibus. Etiam consequat ac
-          augue at dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      <h3 id="fermin" class="mx-10 text-3xl text-left">Sobre Fermín</h3>
+      <section class="container w-100 my-8 flex mx-auto flex-col text-justify gap-6 text-lg sm:px-20">
+        <p class="text-2xl text-gray-600">
+          Fermín, un ciudadano ruso, emigró a Andalucía en busca de un nuevo comienzo, escapando de las adversidades de su
+          tierra natal. En Rusia, la vida era dura, y enfrentaba problemas inusuales con los osos, que eran una amenaza
+          constante para él y su comunidad. Esta lucha constante contra la naturaleza hostil de Rusia forjó su carácter y
+          su resistencia, preparándolo para desafíos aún mayores en su futuro.
         </p>
-        <p>
-          Suspendisse potenti. Nunc vitae suscipit leo. Cras rutrum tempor cursus. Morbi interdum libero ac volutpat
-          efficitur. Vestibulum turpis turpis, euismod at augue id, condimentum sollicitudin ligula. Mauris a nisl ac nisl
-          malesuada viverra a non justo. Donec et turpis placerat, congue nunc id, auctor sem. Sed placerat turpis a risus
-          dapibus ullamcorper non ut nisl. Nunc ornare lorem sem, et egestas mauris cursus id. Donec gravida lorem eros,
-          in vulputate velit blandit ac. Pellentesque congue elementum ante id fringilla. Aliquam erat volutpat.
+        <p class="text-2xl text-gray-600">
+          Cuando llegó a Málaga, encontró un refugio en la cultura española, donde la fiesta y la celebración eran parte
+          fundamental de la vida. La transición de la dureza rusa a la alegría andaluza fue un contraste asombroso, y
+          Fermín se sumergió en esta nueva forma de vida con entusiasmo. Aquí, descubrió el valor de disfrutar el momento
+          y compartir momentos de alegría con otros.
         </p>
-        <p>
-          Donec non erat vitae leo ultrices cursus. Vivamus consequat leo vitae feugiat porta. Pellentesque maximus
-          consequat nulla sed aliquet. Integer volutpat purus vitae odio ullamcorper, mattis feugiat sem ullamcorper.
-          Nullam tempus sem imperdiet, accumsan nisl quis, pellentesque lacus. Donec vestibulum ultricies eros, eu
-          pellentesque eros auctor vel. Sed vehicula dui lorem, ut luctus metus mattis efficitur.
+        <p class="text-2xl text-gray-600">
+          Sin embargo, su experiencia anterior en Rusia nunca se desvaneció por completo. En lugar de olvidarla, Fermín la
+          utilizó como motivación para involucrarse en la vida política. Vio similitudes en la necesidad de resistir y
+          enfrentar desafíos tanto en su tierra natal como en España. Su historia personal de superar obstáculos y
+          encontrar la alegría en la vida se convirtió en la base de su compromiso político.
         </p>
-        <p>
-          Morbi vel fermentum nibh, et vestibulum odio. Quisque quis dolor sit amet tortor pellentesque pretium sit amet
-          non mauris. Nullam cursus felis vel rutrum dapibus. Sed non iaculis diam, ac ultricies turpis. Quisque porttitor
-          ullamcorper nulla id malesuada. Phasellus ac neque ut metus elementum suscipit at ac neque. Fusce auctor auctor
-          ex id pretium. Nunc ultricies egestas tincidunt. Aliquam blandit lorem non nibh tristique vestibulum.
+        <p class="text-2xl text-gray-600">
+          Fermín canalizó su pasión y determinación en la política, abogando por políticas que apoyaran el trabajo duro y
+          la igualdad, inspirado por su historia de vida única que abarcaba desde la dura Rusia hasta la alegre Andalucía.
         </p>
-        <p>
-          Sed eu nulla id turpis ullamcorper mattis eget at massa. Sed congue tortor sit amet leo sodales facilisis.
-          Vestibulum ultricies at metus non gravida. Ut lorem odio, aliquam sit amet faucibus vitae, facilisis posuere
-          odio. Sed sollicitudin velit sit amet libero porta venenatis. Integer sagittis quam sed porta scelerisque.
-          Phasellus scelerisque efficitur tellus sit amet posuere. Praesent laoreet congue odio, ut tempus massa porttitor
-          id. Vivamus sapien arcu, consectetur a aliquet ac, ullamcorper ut risus. Quisque placerat feugiat finibus.
-          Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        <p class="text-2xl text-gray-600">
+          En resumen, las vivencias duras de Fermín, desde los problemas con los osos hasta los inviernos rusos
+          implacables, moldearon su carácter y lo convirtieron en un hombre de confianza con un amor por tener la razón y
+          un profundo conocimiento de una amplia variedad de temas. Su historia es una inspiración para todos nosotros.
         </p>
       </section>
 
+      <h3 class="mx-10 text-3xl text-left">Información de Campaña</h3>
+      <section id="info" class="container w-100 my-8 flex mx-auto flex-col text-justify gap-6 text-lg sm:px-20">
+        <p class="text-2xl text-gray-600">
+          Aquí tienes información de la campaña
+        </p>
+      </section>
     </main>
-    <footer class="flex flex-row bg-andalucia-green-light text-white p-4 fixed bottom-0 w-full z-10">
-      <p class="" id="countdown"></p>
+    <footer class="flex flex-row bg-andalucia-green-light text-white md:px-2 py-1 text-sm fixed bottom-0 w-full z-10">
+      Copyright 2024, todos los derechos reservados. Cualquier paecido con la realidad es pura coincidencia.
     </footer>
   </div>
 </template>
@@ -143,5 +154,4 @@ body {
 p {
   margin-top: 10px;
   margin-bottom: 10px;
-}
-</style>
+}</style>
